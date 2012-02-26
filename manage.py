c07ed6c714913f6beddc42edf0becaa7a20d09a0
Alex Toney <toneyalex@gmail.com>
2012-02-25 17:13:01 -0800

    Adding initial code

---
+++ manage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+
+if __name__ == '__main__': 
+    from Admin import app
+    app.debug = True
+    app.run('127.0.0.1', 5003)
