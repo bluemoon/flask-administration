7738597efe5f80e6a38256760e5b9d7c3a8d07e4
Alex Toney <toneyalex@gmail.com>
2012-02-25 18:10:28 -0800

    Adding more crap

--- example/test.py
+++ example/test.py
@@ -1,17 +1,18 @@
 from flask import Flask
+from flask.ext import adminitizer
 
 def create_app(database_uri='sqlite://'):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config['SECRET_KEY'] = 'not secure'
-    #db.init_app(app)
-    #datastore = SQLAlchemyDatastore(
-    #    (Course, Student, Teacher), db.session)
-    admin_blueprint = admin.create_admin_blueprint(datastore)
+    admin_blueprint = adminitizer.create_adminitizer_blueprint()
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
-    #db.create_all(app=app)
 
     @app.route('/')
     def go_to_admin():
         return redirect('/admin')
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Adminitizer',
-    version='0.0.1',
+    version='0.0.7',
     packages=['flask_adminitizer'],
     include_package_data=True,
     zip_safe=False,
