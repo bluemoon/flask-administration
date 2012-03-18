e010601e5f4d012ec6f3f25c00bb6c5ef00c740d
Alex Toney <toneyalex@gmail.com>
2012-03-18 16:14:11 -0700

    Adding the dashboard and bumping the version

---
+++ flask_administration/dashboard.py
@@ -0,0 +1,21 @@
+
+class dashboard(object):
+	""" Base class for the dashboard """
+	pass
+
+class gauge_mixin(object):
+	""" Base mixin for gauge """
+	pass
+
+class gauge(gauge_mixin):
+	""" Base class for the gauge """
+	pass
+
+
+class cluster(object):
+	def __init__(self):
+		pass
+
+class bars(cluster):
+	def __init__(self, autoupdate=60):
+		pass
