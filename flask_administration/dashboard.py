d1a16427e38a9d1b482f2b9ef67633ee5526ce54
Alex Toney <toneyalex@gmail.com>
2012-03-18 17:55:33 -0700

    Fixing the doctest

--- flask_administration/dashboard.py
+++ flask_administration/dashboard.py
@@ -1,84 +1,82 @@
 
 class dashboard(object):
-	""" Base class for the dashboard """
-	
-	def __init__(self, **kwargs):
-		self.clusters = kwargs.get('clusters', [])
+    """ Base class for the dashboard """
+    
+    def __init__(self, **kwargs):
+        self.clusters = kwargs.get('clusters', [])
 
-	def __iadd__(self, other):
-		""" This should support adding in a manner like this::
+    def __iadd__(self, other):
+        """ This should support adding in a manner like this::
 
-		   >>> b = bars()
-		   >>> d = dashboard()
-		   >>> d += b
-		"""
-		if isinstance(other, cluster):
-			self.clusters.append(other)
-		return self
+           >>> b = bars()
+           >>> d = dashboard()
+           >>> d += b
+        """
+        if isinstance(other, cluster):
+            self.clusters.append(other)
+        return self
 
 
 class gauge_mixin(object):
-	""" Base mixin for gauge """
-	pass
+    """ Base mixin for gauge """
+    pass
 
 
 class gauge(gauge_mixin):
-	""" Base class for the gauge """
-	def __init__(self, **kwargs):
-		self.mongo = kwargs.get('mongo_cls')
+    """ Base class for the gauge """
+    def __init__(self, **kwargs):
+        self.mongo = kwargs.get('mongo_cls')
 
 
 class cluster(object):
-	def __init__(self, gauges, **kwargs):
-		""" cluster initialization
+    def __init__(self, **kwargs):
+        """ cluster initialization
 
-		:param gauges: A list of gauges or a single element
-		:param **kwargs: keyworded arguments
+        :param gauges: A list of gauges or a single element
+        :param **kwargs: keyworded arguments
 
-		"""
-		pass
+        """
+        self.gauges = kwargs.get('gauges', [])
+        #: Autoupdate defaults to 1000ms and this value should be in ms
+        self.autoupdate = kwargs.get('autoupdate', 1000)
 
-	@property
-	def data(self):
-		""" Some documentation text.
 
-		.. doctest::
+    @property
+    def data(self):
+        """ Some documentation text.
 
-		    >>> print 1
-			1
 
-		Some more documentation text. """
-		pass
+        >>> print 1
+        1
+
+        Some more documentation text. """
+        pass
 
 
 class bars(cluster):
-	def __init__(self, gauges, **kwargs):
-		""" Bar graph cluster
+    def __init__(self, **kwargs):
+        """ Bar graph cluster
 
-		:param gauges: A list of gauges or a single element
-
-		:param **kwargs: keyworded arguments
-
-		"""
-		self.gauges = gauges
-		#: Autoupdate defaults to 1000ms and this value should be in ms
-		self.autoupdate = kwargs.get('autoupdate', 1000)
-
+        :param **kwargs: keyworded arguments
+        """
+        pass
+        
 
 class top_list(cluster):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	@property
-	def data(self):
-		pass
+    @property
+    def data(self):
+        pass
 
 
 
 def dashboard_test():
-	""" Build from classes """
-	dash = dashboard()
-	dash += bars()
+    """ Build from classes """
+    dash = dashboard()
+    dash += bars()
 
-if __name__ == '__main__':
-	dashboard_test()
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
