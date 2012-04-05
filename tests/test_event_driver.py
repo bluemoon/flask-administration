ed73a8b6af17cf32f8330463d8fc5cd2cf90aeae
Alex Toney <toneyalex@gmail.com>
2012-04-04 19:14:43 -0700

    Missed another typo

--- tests/test_event_driver.py
+++ tests/test_event_driver.py
@@ -5,7 +5,7 @@ from flask.ext.administration import metrics
 class EventDriverTestCase(unittest.TestCase):
     def setUp(self):
         app = Flask(__name__)
-        app.register_blueprint(event_driver.event_blueprint)
+        app.register_blueprint(metrics.event_blueprint)
         self.app = app.test_client()
 
     def test_empty_db(self):
